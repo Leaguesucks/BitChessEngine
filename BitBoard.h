5f157e8c296891a81e11d8f80129bdc55f60a8db
Dang Nguyen <Bidang2003@gmail.com>
2025-02-05 12:51:37 -0700

    Initial Commit: Define a BitBoard

---
+++ BitBoard.h
@@ -0,0 +1,35 @@
+#ifndef _BITBOARD_H_
+#define _BITBOARD_H_
+
+#define BLACK 0
+#define WHITE 1
+
+#define RIGHT 0
+#define LEFT  1
+
+#define UP    0
+#define DOWN  1
+
+typedef unsigned char      U8 ;
+typedef unsigned short int U16;
+typedef unsigned       int U32;
+typedef unsigned long  int U64;
+
+typedef struct {
+    U64 wPawns;
+    U64 wKnights;
+    U64 wBishops;
+    U64 wRooks;
+    U64 wQueens;
+    U64 wKing;
+
+    U64 bPawns;
+    U64 bKnights;
+    U64 bBishops;
+    U64 bRooks;
+    U64 bQueens;
+    U64 bKing;
+} BitPiece;
+
+
+#endif
