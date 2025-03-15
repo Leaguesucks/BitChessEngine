cdced8542a170bec093f341f45be0ee37f398d05
Dang Nguyen <Bidang2003@gmail.com>
2025-03-14 22:15:58 -0600

    GUI Initial Commit

---
+++ scrs/GUI/ChessGUI.c
@@ -0,0 +1,32 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+/* INITAL COMMIT: An SDL2 example */
+int main(int argc, char *argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    // Wait for the user to close the window
+    SDL_Event e;
+    int quit = 0;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
