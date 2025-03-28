6a581f7bba5c69fa804d453daba9143e28b8ea1f
Dang Nguyen <Bidang2003@gmail.com>
2025-03-27 19:45:42 -0600

    Add BookOpenings.log

--- Test/debug.c
+++ Test/debug.c
@@ -5,13 +5,15 @@
 int main(void) {
     srand(time(NULL));
 
-    while (1) {
-        U64 b = random_U64_with_more_1sBit();
-        PrintBoard(b, stdout);
-        printf("\nLSMB 1 bits: %s", squares[Get_LSMB(b ,1)]);
-        printf("\nLSMB 0 bits: %s\n", squares[Get_LSMB(b ,0)]);
-        getchar();
-    }
+    // while (1) {
+    //     U64 b = random_U64_with_more_1sBit();
+    //     PrintBoard(b, stdout);
+    //     printf("\nLSMB 1 bits: %s", squares[Get_LSMB(b ,1)]);
+    //     printf("\nLSMB 0 bits: %s\n", squares[Get_LSMB(b ,0)]);
+    //     getchar();
+    // }
+
+    FEN_Debug(stdout);
 
     return 0;
 }
